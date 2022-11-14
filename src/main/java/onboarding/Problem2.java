cee4642f0d7d546469c83cb44d58f2245ac88f6d
ahnsucheol <josephscahn@gmail.com>
2022-11-14 20:03:35 +0900

    Problem2, 3 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        String[] cryptoArr = cryptogram.split("");
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push(cryptoArr[0]);
+
+        for(int i=1; i < cryptoArr.length; i++) {
+            System.out.println(stack);
+            if(!cryptoArr[i].equals(stack.peek())){
+                stack.push(cryptoArr[i]);
+            } else {
+                stack.pop();
+            }
+        }
+        System.out.println(stack);
+
+        answer = String.join("",stack);
         return answer;
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++){
+            String numStr = new String();
+            numStr = Integer.toString(i);
+            String[] numArr;
+            numArr = numStr.split("");
+            for (String el : numArr) {
+                if(el.equals("3") || el.equals("6") || el.equals("9")){
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
